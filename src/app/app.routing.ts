13234a7627ccf2605b2f0b304ddb07ef4cb5642b
estudiante <estudiante@ISIS2603-388.sis.virtual.uniandes.edu.co>
2018-10-18 10:21:15 -0500

    con path

---
+++ src/app/app.routing.ts
@@ -0,0 +1,4 @@
+import {NgModule} from '@angular/core/';
+
+import {RouterModule, Routes} from '@angular/router';
+import{EspecialistaComponent} from '@especialista/especialista-list/especialista-list.component';
