1ef27693b11d538aae0e935189d888e94b343de2
Leidy Romero <lj.romero@uniandes.edu.co>
2018-11-06 08:28:14 -0500

    correccion de los componentes de listar y modulos de deportista y objetivo

--- src/app/deportista/deportista-list/deportista-list.component.ts
+++ src/app/deportista/deportista-list/deportista-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Deportista } from '../deportista';
 import { DeportistaService } from '../deportista.service';
+import {DeportistaDetail} from '../deportista-detail';
 
 @Component({
   selector: 'app-deportista-list',
@@ -12,11 +13,37 @@ export class DeportistaListComponent implements OnInit {
   constructor(private deportistaService:DeportistaService) { }
 
  deportistas : Deportista[];
- 
+ selectedDeportista : Deportista;
+ deportista_id: number;
+ showCreate: boolean;
+   
+onSelected(deportista_id: number):void {
+        this.showCreate = false;
+        this.deportista_id = deportista_id;
+    this.selectedDeportista = new DeportistaDetail();
+    this.getDeportistaDetail();
+}   
+   
+showHideCreate(): void {
+     if (this.selectedDeportista) {
+               this.selectedDeportista = undefined;
+               this.deportista_id = undefined;
+        }
+        this.showCreate = !this.showCreate;
+    }
+    getDeportistaDetail(): void {
+         this.deportistaService.getDeportistaDetail(this.deportista_id)
+            .subscribe(selectedDeportista => {
+                this.selectedDeportista = selectedDeportista
+            });
+    }
  getDeportistas(): void{
      this.deportistaService.getDeportistas().subscribe(deportistas => this.deportistas = deportistas);
  }
   ngOnInit() {
+      this.showCreate = false;
+      this.selectedDeportista = undefined;
+      this.deportista_id = undefined;
       this.getDeportistas();
   }
 }
--- src/app/objetivo/objetivo-list/objetivo-list.component.ts
+++ src/app/objetivo/objetivo-list/objetivo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Objetivo } from '../objetivo';
 import { ObjetivoService } from '../objetivo.service';
+import {ObjetivoDetail} from '../objetivo-detail';
 
 @Component({
   selector: 'app-objetivo-list',
@@ -9,14 +10,39 @@ import { ObjetivoService } from '../objetivo.service';
 })
 export class ObjetivoListComponent implements OnInit {
   objetivos: Objetivo[];
-  
+  objetivo_id: number;
+  selectedObjetivo : Objetivo;
+  showCreate: boolean;
+   
   constructor(private objetivoService:ObjetivoService) { }
 
   getObjetivos(): void
   {
       this.objetivoService.getObjetivos().subscribe(objetivos => this.objetivos = objetivos);
   }
+  onSelected(objetivo_id: number):void {
+    this.showCreate = false;
+    this.objetivo_id = objetivo_id;
+    this.selectedObjetivo = new ObjetivoDetail();
+    this.getObjetivoDetail();
+}  
+showHideCreate(): void {
+     if (this.selectedObjetivo) {
+               this.selectedObjetivo = undefined;
+               this.objetivo_id = undefined;
+        }
+        this.showCreate = !this.showCreate;
+    }
+    getObjetivoDetail(): void {
+         this.objetivoService.getObjetivoDetail(this.objetivo_id)
+            .subscribe(selectedObjetivo => {
+                this.selectedObjetivo = selectedObjetivo
+            });
+    }
   ngOnInit() {
+      this.showCreate = false;
+      this.selectedObjetivo = undefined;
+      this.objetivo_id = undefined;
       this.getObjetivos();
   }
 
